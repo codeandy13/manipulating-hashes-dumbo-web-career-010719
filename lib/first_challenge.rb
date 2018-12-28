8a6d6bfb5dcc5275e8242012e59846c688b7be21
Andrew Kim <akim618@gmail.com>
2018-12-28 23:57:27 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,6 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
-  contacts
+  contacts[:Freddy Mercury][:favorite_icecream_flavors].delete_if("strawberry")
 end
 
